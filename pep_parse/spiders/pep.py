b2c85165a7e195b9ba23f33f5c7f3cfa67004b13
alex <alexmal24122004@gmail.com>
2024-09-14 01:36:29 +0300

    do a part of parse methods

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -8,4 +10,20 @@ class PepSpider(scrapy.Spider):
 
     def parse(self, response):
         pep_urls = response.css('#numerical-index tbody tr td:nth-child(2) a')
-        pass
+        for pep_url in pep_urls:
+            yield response.follow(pep_url, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        num_name_list = response.css('h1.page-title::text').get().split(' – ')
+        number = int(num_name_list[0].split()[1])
+        name = num_name_list[1]
+        status = response.css('dt:contains("Status") + dd abbr::text').get()
+        data = {
+            'number': number,
+            'name': name,
+            'status': status
+        }
+        yield PepParseItem(data)
+
+# дописал возврат items со спарсенными данными,
+# но может в метод parse надо еще что-то добавить ;)
