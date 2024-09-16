8034fcb32a573dddf48e0dc7cebc72b4ab25ecda
alex <alexmal24122004@gmail.com>
2024-09-16 16:34:44 +0300

    done review

--- pep_parse/constans.py
+++ pep_parse/constans.py
@@ -1,4 +1,7 @@
+from csv import unix_dialect
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parents[1]
 FORMAT_DATE = '%Y-%m-%d_%H-%M-%S'
+ENCODING = 'utf-8'
+DIALECT = unix_dialect
--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,27 +1,27 @@
 import csv
 import datetime as dt
 
-from pep_parse.constains import BASE_DIR, FORMAT_DATE
+from pep_parse.constans import BASE_DIR, DIALECT, ENCODING, FORMAT_DATE
 
 
 class PepParsePipeline():
     def open_spider(self, spider):
-        self.count_pep = 0
-        self.count_status = {}
+        self.statuses = {'Статус': 'Количество'}
 
     def process_item(self, item, spider):
         status = item['status']
-        self.count_pep += 1
-        self.count_status[status] = self.count_status.get(status, 0) + 1
+        self.statuses[status] = self.statuses.get(status, 0) + 1
         return item
 
     def close_spider(self, spider):
-        results = list(self.count_status.items())
+        total_count = sum(list(self.statuses.values())[1:])
+        self.statuses['Total'] = total_count
+        results = list(self.statuses.items())
         time = dt.datetime.now().strftime(FORMAT_DATE)
         filename = f'status_summary_{time}.csv'
         filepath = BASE_DIR / 'results' / filename
-        with open(filepath, 'w', encoding='utf-8') as csv_file:
-            writer_obj = csv.writer(csv_file, dialect='unix')
-            writer_obj.writerow(['Статус', 'Количество'])
-            writer_obj.writerows(results)
-            writer_obj.writerow(['Total', str(self.count_pep)])
+        with open(filepath, 'w', encoding=ENCODING) as csv_file:
+            writer_obj = csv.writer(csv_file, dialect=DIALECT)
+            writer_obj.writerow(results[0])
+            writer_obj.writerows(results[1:-1])
+            writer_obj.writerow(results[-1])
